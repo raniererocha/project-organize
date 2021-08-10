3571f64df20f99251e1ef80376522d1231b6bff8
raniererocha <rlsmont123@hotmail.com>
2021-08-10 10:55:50 -0300

    Comentarios adicionados

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import path from "path";
 import start, { iProject } from "./app/worker";
 
 const config: iProject = {
-  name: "backend legal do mmte",
-  project_path: path.resolve(__dirname, "../", "../"),
-  type: "BACKEND",
+  name: "nome do projeto", //auto explicativo, o nome do projeto
+  project_path: path.resolve(__dirname, "../", "../"), //Local da minha pasta de projetos projetos
+  type: "BACKEND", //Aqui é onde se escolhe se vai querer um template vazio, backend ou frontend
 };
 
 start(config);
